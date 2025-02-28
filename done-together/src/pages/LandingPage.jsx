2ae450fe4b0ea7248e9937f739a1cd283eaba065
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2025-02-28 17:04:29 -0500

    fix positioning

--- done-together/src/pages/LandingPage.jsx
+++ done-together/src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router";
+import { Link } from "react-router"; // If you're using `react-router-dom`, this should be `import { Link } from "react-router-dom";`
 
 import Navbar from '../components/Navbar';
 import homeArrow from '../assets/icons/arrowHomePage.svg';
@@ -8,28 +8,30 @@ import "../landingPage.css"
 
 function LandingPage(){
     return (
-        <div className="container">
-          <h1 className="done-together">DoneTogether</h1>
-          <img src={homeArrow} alt="Home Arrow" width="100" height="100" className = "home-page-arrow"/>
-          <div className="goals">Goals.</div>
-          <div className="compete">Compete.</div>
-          <div className="collaborate">Collaborate.</div>
-          <div className="logo">Logo</div>
-          <div className="button">Get Started ➪</div>
-          <div class="navbar">
-            <div className="nav-button nav-about-us">About Us</div>
-            <div className="nav-button nav-log-in">Log In</div>
-            <div className="nav-button nav-sign-up">Sign Up</div>
+        <div>
+          <div className="welcome-container">
+            <h1 className="done-together">DoneTogether</h1>
+            <img src={homeArrow} alt="Home Arrow" className="home-page-arrow"/>
+            <div className="logo">Logo</div>
+            <div className="button">Get Started ➪</div>
+            <div className="navbar">
+              <div className="nav-button nav-about-us">About Us</div>
+              <div className="nav-button nav-log-in">Log In</div>
+              <div className="nav-button nav-sign-up">Sign Up</div>
+            </div>
+            <div className="success-text">Success is better together.</div>
           </div>
-          <div className="success-text">Success is better together.</div>
-          <div className="section-text section-text-1">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
-          <div className="section-text section-text-2">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
-          <div className="section-text section-text-3">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
+
+          <div className="blurbs-container">
+            <div className="goals">Goals.</div>
+            <div className="compete">Compete.</div>
+            <div className="collaborate">Collaborate.</div>
+            <div className="section-text section-text-1">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
+            <div className="section-text section-text-2">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
+            <div className="section-text section-text-3">Ort nur endigend erzahlte spielend hausherr ihr schmales tadellos. Wu preisen so pa argerte gefallt wahrend schonen. Neu</div>
+          </div>
         </div>
-      );
-      
+    );
 }
-
-
 
 export default LandingPage;
