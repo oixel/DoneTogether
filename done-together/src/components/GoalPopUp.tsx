2ba8cad25f90570c78e8f05013cf232f8968eb61
Kriti <kritishah989@gmail.com>
2025-03-05 23:58:32 -0500

    Add create goal name and description

--- done-together/src/components/GoalPopUp.tsx
+++ done-together/src/components/GoalPopUp.tsx
@@ -8,24 +8,37 @@ interface GoalPopUpProps {
 
 const GoalPopUp: React.FC<GoalPopUpProps> = ({ setGoalPopUpState }) => {
   const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
 
   return (
     <div className="pop-up">
       <button onClick={() => setGoalPopUpState(false)}>Exit</button>
       <div className="create-goal">Create Goal</div>
-      <form>
-        <label className='goal-name'>Goal Name: </label>
-        <input className="goal-name-input"
-        type = "text"
-        required
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+      <form className="goal-form">
+        <label className="goal-name">Goal Name:</label>
+        <input
+          className="goal-name-input"
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-       <p>{title}</p>
+
+        <label className="goal-description">Goal Description:</label>
+        <textarea
+          className="goal-description-input"
+          required
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {/* Debugging  */}
+        {/* <p>{description}</p> */}
+
       </form>
     </div>
-    
   );
+
+
 };
 
 export default GoalPopUp;
