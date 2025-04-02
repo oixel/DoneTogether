88518221c76ab7d1c2b46f502bb4c95c92f038a0
carsonful <carsonhcf@gmail.com>
2025-04-02 10:45:05 -0400

    commit msg

--- client/src/components/AddUser.tsx
+++ client/src/components/AddUser.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import "../styles/addUser.css";
 import { FaRegCheckCircle } from "react-icons/fa";
-
+// import { updateUsersList } from '../api/goalRequests.ts'; 
 const AddUserComponent = () => {
   // Local state => should that cause any issues?
   const [addUserPopUpState, setAddUserPopUpState] = useState(false);
+  const [username, setUsername] = useState("");
 
   const handleAddUserClick = () => {
     // Toggle the state to open/close the popup
     setAddUserPopUpState(!addUserPopUpState);
+    if (addUserPopUpState && username) {
+      // If the popup is closing, reset the username
+      console.log("Username added:", username);
+      setUsername("");
+    }
   };
+
+  const handleUserNameChange = (e) => {
+    setUsername(e.target.value);
+  }
 
   return (
     <div>
@@ -22,6 +32,8 @@ const AddUserComponent = () => {
           <input 
             type="text" 
             placeholder="Enter Username" 
+            value={username}
+            onChange={handleUserNameChange}
           />
           <FaRegCheckCircle className='confirm-button' onClick={handleAddUserClick}>Close</FaRegCheckCircle> {/* Close and return back to add user button */}
         </div>
