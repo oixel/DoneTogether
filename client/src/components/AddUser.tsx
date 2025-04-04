e1d2d6815fb6f920f180c20cb513a8dc42a4a6a4
carsonful <carsonhcf@gmail.com>
2025-04-04 10:35:39 -0400

    Add user existence check and integrate with updateUsersList in AddUserComponent

--- client/src/components/AddUser.tsx
+++ client/src/components/AddUser.tsx
@@ -1,8 +1,13 @@
-import  { useState } from 'react';
+import  { JSX, useState } from 'react';
 import "../styles/addUser.css";
 import { FaRegCheckCircle } from "react-icons/fa";
-// import { updateUsersList } from '../api/goalRequests.ts'; 
-const AddUserComponent = () => {
+import "../api/goalRequests.ts"; // Assuming you have a file for API requests
+import { getUserByName} from '../api/userRequests';
+import { updateUsersList } from '../api/goalRequests.ts'; 
+import { UserData } from '../types/userData.ts';
+import { GoalData } from '../types/goalData.ts';
+
+function AddUserComponent({goalId}: {goalId: string}): JSX.Element {
   // Local state => should that cause any issues?
   const [addUserPopUpState, setAddUserPopUpState] = useState(false);
   const [username, setUsername] = useState("");
@@ -12,10 +17,37 @@ const AddUserComponent = () => {
     setAddUserPopUpState(!addUserPopUpState);
     if (addUserPopUpState && username) {
       // If the popup is closing, reset the username
+      const checkUser = checkExistence();
+      if(checkUser != null) {
+        console.log("User exists");
+        //addUserToGoal(checkUser);
+
+        return;
+      }
       console.log("Username added:", username);
       setUsername("");
     }
   };
+
+  const checkExistence = async () => {
+    const user = await getUserByName(username);
+    console.log(user);
+    if (user != null) {
+      const newUser = {
+        userId: user?.userId,
+        joined: false,
+        completed: false
+      }
+
+      console.log(goalId);
+      await updateUsersList(goalId ,newUser, 'add');
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  
 
   const handleUserNameChange = (e) => {
     setUsername(e.target.value);
