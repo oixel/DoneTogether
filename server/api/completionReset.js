98fcc6481388751e8f4a67561c900244963b87ba
oickle <oycell@aol.com>
2025-04-07 16:17:49 -0400

    Further progress

--- server/api/completionReset.js
+++ server/api/completionReset.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const moment = require('moment');
 
 const serverInfoPath = 'config/serverInfo.json';
 
@@ -7,7 +8,7 @@ let previousResetDate;
 
 async function updatePreviousResetDate() {
     // If server information does not exist, create a new one
-    const currentDate = new Date().getUTCDate();
+    const currentDate = moment().format("YYYY-MM-DD");
     await fs.writeFileSync(serverInfoPath, JSON.stringify({ "previousResetDate": currentDate }));
     previousResetDate = currentDate;
 }
@@ -31,15 +32,15 @@ function handleCompletionReset(app, database) {
     // Every five seconds, check whether it is a new day. If it is, reset completion status on goals
     setInterval(async function () {
         // Grabs the current date for comparison
-        const currentDate = new Date().getUTCDate();
+        const currentDate = moment().format("YYYY-MM-DD");
 
         // If the date is no longer a match, reset the completion status of goals
         if (currentDate != previousResetDate) {
-            console.log("Resetting completion status of goals!")
-            // resetCompletion(app, database);
+            await resetCompletion(app, database);
+            console.log(`${currentDate} :: Completion status of goals was reset!`)
 
-            // Update the previous reset date to be today!
-            fs.writeFile("serverInfo.json", JSON.stringify({ "previousResetDate": currentDate }));
+            // Update the previous reset date to be today!     
+            await fs.writeFileSync(serverInfoPath, JSON.stringify({ "previousResetDate": currentDate }));
         }
     }, 5000);
 }
@@ -48,8 +49,9 @@ function handleCompletionReset(app, database) {
 function resetCompletion(app, database) {
     // Do a query check:
     // if reset = "daily"
-    // if reset = current day of week
-    // if reset = day of month (with consideration for days with fewer months)
+    // if reset = current day of week (0-6)
+    // if reset = day of month (01-31)
+    // (with consideration for days with fewer months)
 }
 
 module.exports = { updatePreviousResetDate, initializeCompletionReset, handleCompletionReset }
