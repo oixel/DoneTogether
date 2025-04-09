afe04d2490abc6aa5033d021f8edd87d5ebf2ac1
oickle <oycell@aol.com>
2025-04-09 12:05:15 -0400

    Add reset functionality!

--- server/api/completionReset.js
+++ server/api/completionReset.js
@@ -28,30 +28,52 @@ function initializeCompletionReset() {
     }
 }
 
-function handleCompletionReset(app, database) {
-    // Every five seconds, check whether it is a new day. If it is, reset completion status on goals
-    setInterval(async function () {
-        // Grabs the current date for comparison
-        const currentDate = moment().format("YYYY-MM-DD");
+// 
+async function handleCompletionReset(database) {
+    // Grabs the current date for comparison
+    const currentDate = moment().format("YYYY-MM-DD");
 
-        // If the date is no longer a match, reset the completion status of goals
-        if (currentDate != previousResetDate) {
-            await resetCompletion(app, database);
-            console.log(`${currentDate} :: Completion status of goals was reset!`)
+    // If the date is no longer a match, reset the completion status of goals
+    if (currentDate != previousResetDate) {
+        await resetCompletion(database);
+        console.log(`${currentDate} :: Completion status of goals was reset!`)
 
-            // Update the previous reset date to be today!     
-            await fs.writeFileSync(serverInfoPath, JSON.stringify({ "previousResetDate": currentDate }));
-        }
-    }, 5000);
+        // Update the previous reset date to be today!     
+        await fs.writeFileSync(serverInfoPath, JSON.stringify({ "previousResetDate": currentDate }));
+    }
 }
 
 // 
-function resetCompletion(app, database) {
+async function resetCompletion(database) {
     // Do a query check:
     // if reset = "daily"
     // if reset = current day of week (0-6)
     // if reset = day of month (01-31)
     // (with consideration for days with fewer months)
+
+    // 
+    try {
+        const dayOfWeek = new Date().getUTCDay().toString();
+        const dayOfMonth = new Date().getUTCDate().toString().padStart(2, '0');
+
+        const filter = {
+            $or: [
+                { resetType: "daily" },
+                { resetType: dayOfWeek },
+                { resetType: dayOfMonth }
+            ]
+
+        };
+
+        //
+        var update = { $set: { "users.$[].completed": false } };
+
+        // 
+        await database.collection('goals').updateMany(filter, update);
+    }
+    catch (error) {  // 
+        console.error(`Ran into error while resetting goal completion: ${error}...`);
+    }
 }
 
 module.exports = { updatePreviousResetDate, initializeCompletionReset, handleCompletionReset }
